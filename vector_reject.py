eadedece3b82a977013a777ba15d2d27048df49d
Shirish Pokharel <shirish93@users.noreply.github.com>
2016-02-11 14:54:35 -0500

    Update vector_reject.py

--- vector_reject.py
+++ vector_reject.py
@@ -6,8 +6,12 @@ from numpy import float32 as REAL
 from gensim.models import Word2Vec
 from numpy import array
 from unicodedata import normalize
-model = Word2Vec.load('C:\Users\spokha01\SkyDrive\Public\Public Datasets\Kantipur')
 
+print("libraries loaded")
+model = Word2Vec.load(r'C:\Users\n0272436\Desktop\datasets\Kantipur')
+print("model 1 loaded")
+model1 = Word2Vec.load(r'C:\Users\n0272436\Desktop\datasets\v1.model')
+print ("model 2 loaded")
 def reject(A,B):
   #RAW REJECT, REJECT VECTOR B FROM VEC -A
   '''Create a 'projection', and subract it from the original vector'''
@@ -31,8 +35,20 @@ def reject_words(A, B):
   result = [(model.index2word[sim], float(dists[sim])) for sim in best if model.index2word[sim] not in in_words]
   return result
 
+def createTranspose(word):
+  trans =  ['ु', 'ू', 'ि', 'ी']
+  words = []
+  for i, each in enumerate(trans):
+    if i% 2 ==0:
+      words.append(word.replace(each, trans[i+1]))
+    else:
+      words.append(word.replace(each, trans[i-1]))
 
-def reject_words_1(A, B):
+  return list(set(words))
+
+#def compareModels(A, B):
+    
+def reject_words_1(A, B, model = model):
   '''Takes two **LIST OF WORDS** and
   returns most_similar for word A, while rejecting words with meanings closer to B.
   Seems to work better than just giving in negative words.
@@ -47,20 +63,37 @@ def reject_words_1(A, B):
   best  = matutils.argsort(dists, topn = 500, reverse = True)
   result = [(model.index2word[sim], float(dists[sim])) for sim in best if model.index2word[sim] not in in_words]
   return result
-
-def compareWords(wordlist1, wordlist2):
+from tabulate import tabulate
+def compareWords(wordlist1, wordlist2, multimodal = False):
   regular_sim = model.most_similar(wordlist1, negative = wordlist2, topn=200)[:20]
-  rejectWords = reject_words(wordlist1, wordlist2)[:20]
-  rejectWords1 = reject_words_1(wordlist1, wordlist2)[:20]
+  #rejectWords = reject_words_1(wordlist1, wordlist2, model)[:20]
+  rejectWords1 = reject_words_1(wordlist1, wordlist2, model)[:20]
 
-  printJustified(['regular_sim']+list(list(zip(*regular_sim))[0]), ['reject_syn0']\
-                 +list(list(zip(*rejectWords))[0]),
-                 ['reject_syn0norm']+list(list(zip(*rejectWords1))[0]))
-  
-def printJustified(res1, res2, res3):
-  zp = zip(res1, res2, res3)
+  if not multimodal:
+      printJustified(['regular_sim']+list(list(zip(*regular_sim))[0]),\
+                     ['reject_syn0norm']+list(list(zip(*rejectWords1))[0]))
+  else:
+      regular_sim_1 = model1.most_similar(wordlist1, negative = wordlist2, topn=200)[:20]
+      rejectWords1_1 = reject_words_1(wordlist1, wordlist2, model1)[:20]
+      print(\
+        tabulate({"mod0_regular_sim": list(list(zip(*regular_sim))[0]),\
+                "mod0_reject_syn0norm": list(list(zip(*rejectWords1))[0]),\
+                'mod1_regular_sim':list(list(zip(*regular_sim_1))[0]), \
+                'mod1_reject_syn0norm' : list(list(zip(*rejectWords1_1))[0])},\
+               headers = 'keys')
+        )
+      #printJustified(['_mod0_regular_sim']+list(list(zip(*regular_sim))[0]),\
+      #               ['reject_syn0norm']+list(list(zip(*rejectWords1))[0]),\
+      #               ['_mod1_regular_sim']+list(list(zip(*regular_sim_1))[0]),\
+      #               ['_mod1_reject_syn0norm']+list(list(zip(*rejectWords1_1))[0]),\
+      #               ) हावा
+
+
+      
+def printJustified(res1, res2, res3, res4):
+  zp = zip(res1, res2, res3, res4)
   for each in zp:
     l1 = len(each[0])-len(normalize('NFD', each[0]))
     l2 = len(normalize('NFD', each[1]))
     l3 = len(normalize('NFD', each[2]))
-    print ('| '+each[0].ljust(15)+'|'+ each[1].ljust(15)+'| '+each[2].ljust(15)+' |')
+    print ('| '+each[0].ljust(15)+'|'+ each[1].ljust(15)+'| '+each[2].ljust(15)+' |'+each[3].ljust(15))
